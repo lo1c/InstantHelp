b4f1e473af71c2b0d3f39084ef391f61c9b58120
SamuelMarfo <Samuelmarfo.it@gmail.com>
2017-06-10 12:37:57 +0200

    Nav bar fix

--- Client/Web/login.php
+++ Client/Web/login.php
@@ -21,13 +21,13 @@
             </div>
             <ul class="nav navbar-nav">
               <li><a href="index.php">Home</a></li>
-              <li><a href="#">Konzept</a></li>
+              <li><a href="concept.php">Konzept</a></li>
               <li><a href="#">Test</a></li>
-              <li><a href="#">Über Uns</a></li>
+              <li><a href="about.php">Über Uns</a></li>
             </ul>
               <ul class="nav navbar-nav navbar-right">
-                  <li><a href="#"><span class="glyphicon glyphicon-user"></span> Registrieren</a></li>
-                <li class="active"><a href="#"><span class="glyphicon glyphicon-log-in"></span> Login</a></li>
+                  <li><a href="register.php"><span class="glyphicon glyphicon-user"></span> Registrieren</a></li>
+                <li class="active"><a href="login.php"><span class="glyphicon glyphicon-log-in"></span> Login</a></li>
             </ul>
           </div>
         </nav>
