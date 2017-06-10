bceeec89864e781d5d7d7287766f9a577ab87beb
PaulKirchbichler <paul_hamburg@gmx.de>
2017-06-10 15:50:58 +0200

    syntaxerror verbessert interface

--- Client/Web/interface.php
+++ Client/Web/interface.php
@@ -1,7 +1,7 @@
 <?php
 	if(isset($_POST['username']) && isset($_POST['password']) && isset($_POST['repeat']) && isset($_POST['name']) && isset($_POST['surname']) && isset($_POST['phone']) && isset($_POST['email']) && isset($_POST['birthday'])) {
 		try {	
-			$content = $_POST['username'] . "#" . $_POST['password'] . "#" . $_POST['name'] . "#" . $_POST['surname'] . "#" . $_POST['phone'] . "#" . $_POST['email'] . "#" . $_POST['birthday']
+			$content = $_POST['username'] . "#" . $_POST['password'] . "#" . $_POST['name'] . "#" . $_POST['surname'] . "#" . $_POST['phone'] . "#" . $_POST['email'] . "#" . $_POST['birthday'];
 		
 			$handle = fopen("users/" . $_POST['username'] . ".txt", w);
 			fwrite ($handle, $content);
