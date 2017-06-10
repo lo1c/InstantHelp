bb4f7fb57c01648bccd46431a7cf2880b447c055
SamuelMarfo <Samuelmarfo.it@gmail.com>
2017-06-10 12:51:24 +0200

    Web Fav-Fix

--- Client/Web/register.php
+++ Client/Web/register.php
@@ -1,7 +1,8 @@
 <html>
 	
 	<head>
-		<!-- Latest compiled and minified CSS -->
+		<title>Instant.Help</title>
+        <!-- Latest compiled and minified CSS -->
 		<link rel="stylesheet" href="https://maxcdn.bootstrapcdn.com/bootstrap/3.3.7/css/bootstrap.min.css" integrity="sha384-BVYiiSIFeK1dGmJRAkycuHAHRg32OmUcww7on3RYdg4Va+PmSTsz/K68vbdEjh4u" crossorigin="anonymous">
 
 		<!-- Optional theme -->
@@ -9,6 +10,11 @@
 
 		<!-- Latest compiled and minified JavaScript -->
 		<script src="https://maxcdn.bootstrapcdn.com/bootstrap/3.3.7/js/bootstrap.min.js" integrity="sha384-Tc5IQib027qvyjSMfHjOMaLkfuWVxZxUPnCJA7l2mCWNIpG9mGCD8wGNIcPD7Txa" crossorigin="anonymous"></script>
+        
+        <!--favicon-->
+        <link rel="apple-touch-icon-precomposed" sizes="180x180" href="src/graphics/favicon.png">
+        <link rel="shortcut icon" type="image/x-icon" href="src/graphics/favicon.ico">
+        <link rel="shortcut icon" href="src/graphics/favicon.ico">
 	</head>
 	
 	<body>
