5d5dbe2e7cc87b0e0f5723ec6f23a77dc22025aa
PaulKirchbichler <paul_hamburg@gmx.de>
2017-06-10 10:58:23 +0200

    grundlagen erstellen
    grundlagen erstellen

---
+++ Client/Web/index.php
@@ -0,0 +1,39 @@
+<!--
+Titel: index Seite
+Änderungsdatum: 16.06.2016
+Geändert bei: Paul K:
+Kurze Erläterung der Änderung:
+
+
+-->
+
+<!DOCTYPE html>
+<html>
+	<head>
+		<title>InstantHelp</title>
+		<link type="text/css" rel="stylesheet" href="src/css/reset.css">
+		<link type="text/css" rel="stylesheet" href="src/css/style.css">
+		<link href='https://fonts.googleapis.com/css?family=Lato:400,300' rel='stylesheet' type='text/css'>
+		<meta charset="utf-8">
+	</head>
+	<body>
+		<div id="page">
+			<div id="header">
+                <div class="icons">
+				    <h1 class="title">SKAP</h1>
+				    <h6 class="suptitle">SmartKey Administration Panel</h6>
+                </div>
+            </div>
+            
+            <div id="all">
+                <div class="nav">
+                    <div class="navbar" >
+                        <button class="menue">Lol</button>
+                    </div>  
+                
+                
+                </div>
+            </div>
+        </div>
+    </body>
+</html>
