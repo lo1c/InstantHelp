493073dbbb1cdc343468d1e5b898a7d604e90d34
PaulKirchbichler <paul_hamburg@gmx.de>
2017-06-10 20:05:18 +0200

    textart angleichung

--- Client/Web/concept.php
+++ Client/Web/concept.php
@@ -2,6 +2,8 @@
 	
 	<head>
 		<title>Instant.Help</title>
+        
+        <link rel="stylesheet" href="src/css/style.css">
         <!-- Latest compiled and minified CSS -->
 		<link rel="stylesheet" href="https://maxcdn.bootstrapcdn.com/bootstrap/3.3.7/css/bootstrap.min.css" integrity="sha384-BVYiiSIFeK1dGmJRAkycuHAHRg32OmUcww7on3RYdg4Va+PmSTsz/K68vbdEjh4u" crossorigin="anonymous">
 
